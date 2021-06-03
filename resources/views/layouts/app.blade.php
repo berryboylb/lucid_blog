bb65ad1ce87c609ca11a0192001911ea26297f42
berryboylb <phemmynesce4life@gmail.com>
2021-06-03 16:18:38 +0100

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css"/>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <header>
+        @include('partials.header')
+    </header>
+
+             @yield('content')
+
+    {{-- <footer>
+        @include('layouts.footer')
+     </footer> --}}
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<nav>
+    <div class="title"><h3>Navbar</h3></div>
+        <ul class="navlist">
+            <li><a href="">Home</a></li>
+            <li><a href="">Explore</a></li>
+            <li><a href="#"><i class="fas fa-bell"></i></a></li>
+            <li><a href="#"><img src="" alt=""></i></a></li>
+            <a href="#">Signup</a>
+        </ul>
+    <div class="menu">
+        <div class="line0"></div>
+        <div class="line1"></div>
+        <div class="line2"></div>
+    </div>
+</nav>
