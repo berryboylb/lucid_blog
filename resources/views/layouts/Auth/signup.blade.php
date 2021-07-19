77254a27aba2273967647487100da8094d816bc0
berryboylb <phemmynesce4life@gmail.com>
2021-07-19 11:16:42 +0100

    broke boy commit

--- resources/views/layouts/Auth/signup.blade.php
+++ resources/views/layouts/Auth/signup.blade.php
@@ -34,6 +34,8 @@
                 <h3>Already have an account Login here. <a href="">Login</a></h3>
             </form>
         </div>
+        <img class="signup-blobs" src="{{ asset('images/blob (4).svg') }}" alt="">
+         <img class="signup-blob-2" src="{{ asset('images/blob (4).svg') }}" alt="">
     </div> 
    
         <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>    
